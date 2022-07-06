4bbab001562b75c918aa142d18319fa5754f2975
RafLaf <ralafarg@gmail.com>
2022-07-06 15:22:09 +0200

    crop mscoco

---
+++ datasets/metadatasets/mscoco/RUN_ONLY_ONCE.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "RUN THIS SCRIPT TO GET THE CROPPED IMAGES FROM MSCOCO AND THE JSON FILE ASSOCIATED WITH IT (at the image level not the class level) \n It's very long run it only once"
+helpFunction()
+{
+   echo "RUN THIS SCRIPT TO GET THE CROPPED IMAGES FROM MSCOCO AND THE JSON FILE ASSOCIATED WITH IT"
+   echo "Usage: $0 -a dataset_path "
+   echo -e "\t-a dataset_path"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "a:" opt
+do
+   case "$opt" in
+      a ) dataset_path="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$dataset_path" ] 
+then
+   echo "the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+mkdir $dataset_path/metadatasets/mscoco/cropped
+echo folder $dataset_path/metadatasets/mscoco/cropped created ! 
+
+python crop_mscoco.py $dataset_path
+
+
+
