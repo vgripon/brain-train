6094f5f889989e72f1e444774ff5feb33bebc582
nicofarr <nicofarr09@gmail.com>
2022-08-22 15:16:04 +0200

    added CNN with attentional similarity

--- backbones1d.py
+++ backbones1d.py
@@ -1,7 +1,16 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from args import args
 import random # for manifold mixup
+import torchaudio.transforms as T
+
+#### Mel Spectrogram
+
+N_FFT = 1024
+N_MELS = 64
+
+melspec = T.MelSpectrogram(n_fft=N_FFT, n_mels=N_MELS)
 
 class ConvBN1d(nn.Module):
     def __init__(self, in_f, out_f, kernel_size = 9, stride = 1, padding = 4, groups = 1, outRelu = False, leaky = False):
@@ -173,6 +182,78 @@ class ResNet12(nn.Module):
         y = y.mean(dim = list(range(2, len(y.shape))))
         return y
 
+### model ProtNet_att adapted from https://github.com/kevinco27/attentional-similarity/blob/master/main/net/protnet_att.py
+### https://arxiv.org/abs/1812.01269 
+class GLU(nn.Module):
+    def __init__(self, inp, out, ms=(4,4), ds=1):
+        super(GLU, self).__init__()
+        fs = (3,3)
+        ps = (1,1)
+        self.ms = ms
+        self.cnn_lin = nn.Conv2d(inp, out, fs, dilation=ds, padding=ps, bias=False)
+        self.bn = nn.BatchNorm2d(out)
+        self.mp = nn.MaxPool2d(ms)
+
+    def forward(self, x):
+        out = F.relu(self.bn(self.cnn_lin(x)))
+        return self.mp(out)
+
+
+class ProtNet_att(nn.Module):
+    def __init__(self,nfeat=128):
+        super(ProtNet_att, self).__init__()
+        self.model_name = 'ProtNet_att'
+
+        ## Mel Spectrogram extraction and normalization 
+        self.melspec = melspec
+        self.normalize = nn.BatchNorm2d(1)
+
+        ks = nfeat
+        self.G1 = GLU(   1, ks*1)
+        self.G2 = GLU(ks*1, ks*2)
+        self.G3 = GLU(ks*2, ks*3, (1,1))
+        self.att = nn.Conv2d(ks*3, ks*3, (3,3), padding=(1,1), bias=False)
+
+    def nn_att(self, inp, att):
+        att_out = att(inp)  # (130, 384, 8, 10)
+        att_out = F.softmax(att_out.view(att_out.size(0), att_out.size(1), -1), dim=2)  # (130, 384, 80)
+
+        att_sc = att_out.sum(1).view(att_out.size(0), 1, att_out.size(2))  # (130, 1, 80)
+        att_sc = att_sc.div(att_out.size(1))
+        att_sc = att_sc.repeat(1, att_out.size(1), 1)  # (130, 384, 80)
+        return att_sc
+
+    def forward(self, x,mixup = None,lbda = None, perm = None):
+        with torch.no_grad():
+            zx = self.normalize(self.melspec(x)) # (130, 1, 128, 160)
+        G1 = self.G1(zx)  # (130, 128, 32, 40)
+        G2 = self.G2(G1)  # (130, 256, 8, 10)
+        G3 = self.G3(G2)  # (130, 384, 8, 10)
+        #torch.mean(G3,dim=(2,3),keepdim=False)
+        att = self.nn_att(G3, self.att)
+        embed = G3.view(G3.size(0), G3.size(1), -1) * att # (130, 384, 80)
+        embed = embed.sum(-1)  # (130, 384)
+        ### This should correspond to (batch,embedding size) -> output for few shot 
+        return embed
+
+    def forward_protonet(self, x, xavg, xstd, n=5, m=5):
+        zx = (x - xavg) / xstd  # (130, 1, 128, 160)
+        G1 = self.G1(zx)  # (130, 128, 32, 40)
+        G2 = self.G2(G1)  # (130, 256, 8, 10)
+        G3 = self.G3(G2)  # (130, 384, 8, 10)
+        att = self.nn_att(G3, self.att)
+        embed = G3.view(G3.size(0), G3.size(1), -1) * att  # (130, 384, 80)
+        embed = embed.sum(-1)  # (130, 384)
+        ### This should correspond to (batch,embedding size) -> output for few shot 
+
+        embed2 = embed.view(-1, n * m + 1, embed.size(1))  # (5, 26, 384)
+        # query -> (5, 5, 384)
+        query = embed2[:, -1].view(-1, 1, embed2.size(2)).repeat(1, n, 1)
+        # support -> (5, 5, 384)
+        support = embed2[:, :-1].view(embed2.size(0), n, m, -1).mean(2)
+        sim = -torch.pow(query - support, 2).sum(-1)  # (5, 5)
+        return sim
+
 def prepareBackbone():
     return {
         "resnet18": lambda: (ResNet(BasicBlock, [(1, 1, 1), (1, 2, 1.5), (1, 2, 2), (1, 2, 3), (1, 2, 4), (1, 2, 5), (1, 2, 6), (1, 2, 8)], args.feature_maps), 8 * args.feature_maps),
@@ -181,7 +262,8 @@ def prepareBackbone():
         # "resnet56": lambda: (ResNet(BasicBlock, [(9, 1, 1), (9, 2, 2), (9, 2, 4)], args.feature_maps, large = large), 4 * args.feature_maps),
         # "resnet110": lambda: (ResNet(BasicBlock, [(18, 1, 1), (18, 2, 2), (18, 2, 4)], args.feature_maps, large = large), 4 * args.feature_maps),
         # "resnet50": lambda: (ResNet(BottleneckBlock, [(3, 1, 1), (4, 2, 2), (6, 2, 4), (3, 2, 8)], args.feature_maps, large = large), 8 * 4 * args.feature_maps),
-        "resnet12": lambda: (ResNet12(args.feature_maps), 10 * args.feature_maps)
+        "resnet12": lambda: (ResNet12(args.feature_maps), 10 * args.feature_maps),
+        "cnn-protnet": lambda: (ProtNet_att(args.feature_maps), 3*args.feature_maps)
         }[args.backbone.lower()]()
 
 print(" backbones,", end='')
