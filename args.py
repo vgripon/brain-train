d928ff1abec51c9914cb71595991a7a3fa7b3e9c
Vincent Gripon <vincent@gripon.fr>
2022-10-05 15:41:37 +0200

    corrected typo

--- args.py
+++ args.py
@@ -57,7 +57,7 @@ parser.add_argument("--training-dataset", type=str, default="", help="training d
 parser.add_argument("--validation-dataset", type=str, default="", help="validation dataset, overriden by --dataset")
 parser.add_argument("--test-dataset", type=str, default="", help="test dataset, overriden by --dataset")
 parser.add_argument("--dataset-size", type=int, default=0, help="defines a maximum of samples considered at each epoch, 0 means it is ignored")
-parser.add_argument("--audio", action="store_true", help="used audio inputs, so switch back to 1d backbones")
+parser.add_argument("--audio", action="store_true", help="use audio inputs, so switch back to 1d backbones")
 parser.add_argument("--wandb", type=str, default='', help="Report to wandb, input is the entity name")
 parser.add_argument("--wandbProjectName", type=str, default='few-shot', help="wandb project name")
 ### backbones parameters
