989a05685cfe6918e105bc01b9516aa05cc0c3eb
raphael <raphael@pop-os.localdomain>
2022-12-14 13:05:14 +1030

    clean multimachine hyperparameter tuning

--- finetuning/finetune_cub_cluster4.py
+++ finetuning/finetune_cub_cluster4.py
@@ -1,10 +1,38 @@
 import os
 from time import sleep
 os.system('ps aux | grep subdomains.py')
+popos = True
+sl12 = False
+
 N=10   # number of subdomains 
-os.system('python create_dataset_files.py --dataset-path /users2/libre/datasets/ --subdomain /users/local/clusters_imagenet/'+str(N)+'clusters.npy --fast-create-dataset')
-os.system('python main.py --dataset-path /users2/libre/datasets/ --few-shot-shots 5 --few-shot-ways 5 --few-shot-queries 15 --training-dataset metadataset_imagenet_4 --validation-dataset metadataset_cub_validation --test-dataset metadataset_cub_test --epochs 20 --lr 0.05 --few-shot --few-shot-unbalanced-queries --load-backbone /users2/libre/clusters/clusters --save-classifier /users2/libre/raphael/finetune_cubc4_classifier.pt  --backbone resnet12 --freeze-backbone --force-train')# --wandb raflaf --wandbProjectName brain-train' )
-os.system('python main.py --dataset-path /users2/libre/datasets/ --few-shot-shots 5 --few-shot-ways 5 --few-shot-queries 15 --training-dataset metadataset_imagenet_4 --validation-dataset metadataset_cub_validation --test-dataset metadataset_cub_test --epochs 50 --lr 0.0001 --few-shot --few-shot-unbalanced-queries --load-backbone /users2/libre/clusters/clusters --save-backbone /users2/libre/raphael/finetune_cubc4.pt --save-features-prefix /users2/libre/raphael/finetuned_cubc4 --load-classifier /users2/libre/raphael/finetune_cubc4_classifier.pt  --backbone resnet12')# --wandb raflaf --wandbProjectName brain-train' )
-#for i in range(N):
-#    os.system('python main.py --dataset-path /users2/libre/datasets/ --few-shot-shots 5 --few-shot-ways 5 --few-shot-queries 15 --training-dataset metadataset_imagenet_cluster_train_'+str(i)+' --validation-dataset metadataset_aircraft_validation --test-dataset metadataset_vgg_flower_test --epochs 200 --lr 0.1 --cosine --few-shot --few-shot-unbalanced-queries  --save-backbone /users/local/r21lafar/models/randomlargeclusters'+str(i)+' --backbone resnet12')# --wandb raflaf --wandbProjectName brain-train' )
-#    sleep(3)  # time to exit process
+suffix = '--backbone resnet12  --wandb raflaf --wandb-dir wandb --wandbProjectName brain-train --wd 0.0001'
+
+
+if popos: 
+    prefix = 'python main.py --dataset-path /home/datasets/ --load-backbone /home/raphael/Documents/models/resnet12_metadataset_imagenet_64.pt --few-shot-shots 5 --few-shot-ways 5 --few-shot-queries 15 --training-dataset metadataset_imagenet_4 --validation-dataset metadataset_cub_validation --test-dataset metadataset_cub_test --few-shot --few-shot-unbalanced-queries' 
+    dir1 = '/home/raphael/Documents/models/'
+    key = 'optimal'
+    #create dataset_json
+    os.system('python create_dataset_files.py --dataset-path /home/datasets/ --subdomain /home/raphael/Documents/models/'+str(N)+'clusters.npy --fast-create-dataset')
+
+elif sl12:
+    prefix = 'python main.py --dataset-path /users2/libre/datasets/ --load-backbone /users2/libre/clusters/clusters --few-shot-shots 5 --few-shot-ways 5 --few-shot-queries 15 --training-dataset metadataset_imagenet_4 --validation-dataset metadataset_cub_validation --test-dataset metadataset_cub_test --few-shot --few-shot-unbalanced-queries' 
+    dir1 = '/users2/libre/raphael/'
+    suffix = '--backbone resnet12  --wandb raflaf --wandb-dir wandb --wandbProjectName brain-train --wd 0.0001'
+    key = ''
+    #create dataset_json
+    os.system('python create_dataset_files.py --dataset-path /users2/libre/datasets/ --subdomain /users/local/clusters_imagenet/'+str(N)+'clusters.npy --fast-create-dataset')
+    
+
+#get_classifier
+os.system( prefix + ' --freeze-backbone --force-train --epochs 30 --lr 0.1   --save-classifier {0}finetune_cubc4_classifier{1}.pt  {2} --batch-size 128'.format(dir1, key, suffix))
+
+#finetuning
+list_lr = [0.5,0.1,0.05,0.01,0.005]
+
+for lr in list_lr:
+    lr_str = str(lr)
+    print(type(lr_str))
+    command = prefix +' --epochs 20 --lr {0}  --save-backbone {1}finetune_cubc4_{0}{3}.pt --save-features-prefix {1}finetuned_cubc4_{0}{3} --save-classifier {1}classifier_finetuned_cubc4_{0}{3} --load-classifier {1}finetune_cubc4_classifier.pt  {2} --batch-size 128'.format(lr_str, dir1, suffix, key)
+    print(command)
+    os.system(command)
