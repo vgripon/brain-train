7d7617a86d893c65f5a3dc76335809bbc799a9c0
raphael <raphael@pop-os.localdomain>
2022-12-20 17:28:10 +1030

    compatibility popos

--- finetuning/get_logit.py
+++ finetuning/get_logit.py
@@ -1,6 +1,13 @@
 import os
 suffix = '--backbone resnet12  --wandb raflaf --wandb-dir wandb --wandbProjectName brain-train --wd 0.0001'
 
-os.system('python main.py --dataset-path /users2/libre/datasets/  --training-dataset metadataset_imagenet_train  --freeze-backbone --force-train --epoch 20 --lr 0.05 --load-backbone /users2/libre/raphael/resnet12_metadataset_imagenet_64.pt --save-classifier classifier_metadataset_imagenet_64.pt --backbone resnet12 '+ suffix)
-for dataset in ['cub', 'aircraft', 'dtd', 'mscoco', 'fungi', 'omniglot', 'traffic_signs', 'vgg_flower']:
-    os.system('python main.py --dataset-path /users2/libre/datasets/  --validation-dataset metadataset_{0}_validation  --freeze-backbone --freeze-classifier --load-classifier /users2/libre/raphael/classifier_metadataset_imagenet_64.pt --load-backbone /users2/libre/raphael/resnet12_metadataset_imagenet_64.pt --save-logits /users2/libre/raphael/logits_{0}_val.pt --backbone resnet12'.format(dataset))
+sl12 = False
+
+if sl12:
+    os.system('python main.py --dataset-path /users2/libre/datasets/  --training-dataset metadataset_imagenet_train  --freeze-backbone --force-train --epoch 20 --lr 0.05 --load-backbone /users2/libre/raphael/resnet12_metadataset_imagenet_64.pt --save-classifier classifier_metadataset_imagenet_64.pt --backbone resnet12 '+ suffix)
+    for dataset in ['cub', 'aircraft', 'dtd', 'mscoco', 'fungi', 'omniglot', 'traffic_signs', 'vgg_flower']:
+        os.system('python main.py --dataset-path /users2/libre/datasets/  --validation-dataset metadataset_{0}_validation  --freeze-backbone --freeze-classifier --load-classifier /users2/libre/raphael/classifier_metadataset_imagenet_64.pt --load-backbone /users2/libre/raphael/resnet12_metadataset_imagenet_64.pt --save-logits /users2/libre/raphael/logits_{0}_val.pt --backbone resnet12'.format(dataset))
+else:
+    os.system('python main.py --dataset-path /home/datasets/  --training-dataset metadataset_imagenet_train  --freeze-backbone --force-train --epoch 20 --lr 0.05 --load-backbone /home/raphael/Documents/models/resnet12_metadataset_imagenet_64.pt --save-classifier /home/raphael/Documents/models/classifier_metadataset_imagenet_64.pt --backbone resnet12 '+ suffix)
+    for dataset in ['cub', 'aircraft', 'dtd', 'mscoco', 'fungi', 'omniglot', 'traffic_signs', 'vgg_flower']:
+        os.system('python main.py --dataset-path /home/datasets/  --validation-dataset metadataset_{0}_validation  --freeze-backbone --freeze-classifier --load-classifier /home/raphael/Documents/models/classifier_metadataset_imagenet_64.pt --load-backbone /home/raphael/Documents/models/resnet12_metadataset_imagenet_64.pt --save-logits /home/raphael/Documents/models/logits_{0}_val.pt --backbone resnet12'.format(dataset))
