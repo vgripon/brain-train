fff4aef13b231ffe29a489a1b3b7e2a991b56eaf
RafLaf <ralafarg@gmail.com>
2023-05-17 08:23:27 +0200

    measure DI2

--- sbatch/zay/DI2/measure.sh
+++ sbatch/zay/DI2/measure.sh
@@ -9,17 +9,17 @@
 #SBATCH --qos=qos_gpu-t3 # QoS
 #SBATCH --hint=nomultithread # desactiver l’hyperthreading
 #SBATCH --account=csb@v100 # comptabilite V100
-#SBATCH --array=0-0
+#SBATCH --array=0-7
 
-
+lr=0.001
 list1=("aircraft" "cub" "dtd" "fungi" "omniglot" "mscoco" "traffic_signs" "vgg_flower")
 # Get the current string from the list based on the task ID
 dat=${list1[$SLURM_ARRAY_TASK_ID]}
 
-load_backbone_base="${WORK}/results/DI/backbones/${dat}"
-load_backbone="${load_backbone_base}/backbones_20_0.01"
-load_backbone="${WORK}/resnet12_metadataset_imagenet_64.pt"
-save_feat="${WORK}/results/DI/features/${dat}/f_baseline"
+load_backbone_base="${WORK}/results/DI2/backbones/${dat}"
+load_backbone="${load_backbone_base}/backbones_${lr}"
+#load_backbone="${WORK}/resnet12_metadataset_imagenet_64.pt"
+save_feat="${WORK}/results/DI2/features/${dat}/f_${lr}"
 few_shot_runs="600"
 dataset_path="${SCRATCH}/"
 test_dataset="metadataset_${dat}_test"
@@ -27,26 +27,7 @@ epoch="1"
 backbone="resnet12"
 batch_size="128"
 few_shot="--few-shot"
-lr=0.001
-mode=$1
 
-if [ "$mode" == "MD" ]; then
-    few_shot_shots="0"
-    few_shot_ways="0"
-    few_shot_queries="0"
-elif [ "$mode" == "1s5w" ]; then
-    few_shot_shots="1"
-    few_shot_ways="5"
-    few_shot_queries="15"
-elif [ "$mode" == "5s5w" ]; then
-    few_shot_shots="5"
-    few_shot_ways="5"
-    few_shot_queries="15"
-else
-    echo "Invalid mode. Please choose between MD, 1s5w, and 5s5w."
-    exit 1
-fi
-save_result="${WORK}/results/DI/result_DI_0.0001_${mode}.pt"
 
 module purge # nettoyer les modules herites par defaut
 #conda deactivate # desactiver les environnements herites par defaut
